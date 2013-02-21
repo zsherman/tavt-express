5d1709104569e084a921f689cc693f5de209ede0
Zach Sherman <zksherm@gmail.com>
2013-02-21 22:17:44 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TavtExpress::Application.config.secret_token = 'da39a8b2985891905f3d8264c23f864193dced5cab46231ff30f58b26a21a8a38068f87114a174a25e348c122439161ef98f9e9b6b1ebd681a86105b62a181e3'
